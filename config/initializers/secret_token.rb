b4580890b07706b32f3d3434eff92a46ea42a903
Chellberg <corinchellberg@gmail.com>
2013-10-13 20:42:35 -0400

    add empty Rails 4.0 installation with RSpec, Capybara, and FactoryGirl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartSpeed::Application.config.secret_key_base = 'bad2de9a9a82d4f7a6e14920a345e9fc06d4162d7c05adda886ba41b16663de6eef21fbe9a3001ff041f868ed35a1843db5d7708fe466f8818527a03a4726337'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmartSpeed::Application.config.session_store :cookie_store, key: '_smart-speed_session'
