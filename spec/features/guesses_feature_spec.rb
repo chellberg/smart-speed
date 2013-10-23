8782edbf061dd707f2f41d2865ddfcd93cd708b6
Corin Chellberg <corinchellberg@gmail.com>
2013-10-22 23:16:46 -0400

    add feature tests for guess creation

--- spec/features/guesses_feature_spec.rb
+++ spec/features/guesses_feature_spec.rb
@@ -7,9 +7,11 @@ describe "Guesses features" do
   
   shared_examples_for "all guesses pages" do
     it { should have_title(full_title(page_title)) }
+    it { should have_link('Home', :href => '/' )}
+    it { should have_link('Submit') }
   end
   
-  describe "Show guesses#index" do
+  describe "All guesses page" do
     before { click_link "All guesses" }
     let(:page_title) { 'All' }
     it_should_behave_like "all guesses pages"
@@ -21,4 +23,37 @@ describe "Guesses features" do
       it { should have_content(guess.speed_limit) }
 
   end
+  
+  describe "New guess page" do
+    before { click_link "Submit" }
+    let(:page_title) { 'Submit' }
+    it_should_behave_like "all guesses pages"
+    
+    it { should have_content("Address") }
+    it { should have_content("Speed limit") }
+    it { should have_content("Speed") }
+    it { should have_content("Notes") }
+    
+    describe "with blank information" do
+      before { click_button "I solemnly swear that this is good advice" }
+      it { should have_content('error') }
+    end
+    
+    describe "with incomplete information" do
+      before { fill_in 'Speed', with: 55
+      click_button "I solemnly swear that this is good advice" }
+      it { should have_content('error') }
+    end
+    
+    describe "with complete information" do
+      before { fill_in 'Speed', with: 55
+               fill_in 'Speed limit', with: 45 
+               fill_in 'Address', with: "809 S Fess Ave 47401" }
+      it "should create a new guess" do
+        expect { click_button "I solemnly swear that this is good advice" }.to change(Guess, :count).by(1)
+      end         
+    end
+  end
+  
+  
 end
